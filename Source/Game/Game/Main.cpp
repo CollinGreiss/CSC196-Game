ed11f8fab76d5e378454ec314bfed648b781bbf0
KillerBot977 <collingreiss223@gmail.com>
2023-07-16 21:02:02 -0600

    7/16

--- Source/Game/Game/Main.cpp
+++ Source/Game/Game/Main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 
 #include "Core/Core.h"
+
 #include "Renderer/Renderer.h"
 #include "Renderer/Model.h"
-#include "Player.h"
+
 #include "Input/InputSystem.h"
+
+#include "Player.h"
+#include "Enemy.h"
+
 #include <thread>
 
 using namespace std;
@@ -21,6 +26,19 @@ int main(int argc, char* argv[]) {
 
     Player player{ 150, kiko::DegToRad(270.0f), { {400, 300}, 0, 3}, {"ship.txt"} };
 
+    std::vector<Enemy> enemies;
+    for (int i = 0; i < 1; i++)
+        enemies.push_back ({
+            150,
+            kiko::DegToRad(270.0f),
+            kiko::Transform { 
+                { kiko::randomf(kiko::g_renderer.GetWidth()), kiko::randomf(kiko::g_renderer.GetHeight()) },
+                kiko::randomf(kiko::TwoPi), 
+                2 
+            },
+           { "ship.txt" }
+        });
+
     bool quit = false;
     while (!quit) {
 
@@ -30,14 +48,20 @@ int main(int argc, char* argv[]) {
         if (kiko::g_inputSystem.GetKeyDown(SDL_SCANCODE_ESCAPE)) quit = true;
 
         player.Update(kiko::g_time.GetDeltaTime());
+        for (auto enemy : enemies)
+            enemy.Update(kiko::g_time.GetDeltaTime());
 
         ///////
 
         kiko::g_renderer.SetColor(0, 0, 0, 0);
         kiko::g_renderer.BeginFrame();
 
-        kiko::g_renderer.SetColor(kiko::random(256), kiko::random(256), kiko::random(256), 255);
+        kiko::g_renderer.SetColor(255, 255, 255, 255);
         player.Draw(kiko::g_renderer);
+        
+        kiko::g_renderer.SetColor(255, 0, 0, 255);
+        for (auto enemy : enemies)
+            enemy.Draw(kiko::g_renderer);
 
         kiko::g_renderer.EndFrame();
 
